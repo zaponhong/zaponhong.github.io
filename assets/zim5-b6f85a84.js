06543d1fe86df4a8f87bcb03bbcb0f9964dabeba
zaponhong <115920385+zaponhong@users.noreply.github.com>
2023-06-23 18:00:09 +0700

    Delete assets directory

--- assets/zim5-b6f85a84.js
+++
@@ -1,1 +0,0 @@
-const s="/assets/zim1-1646850b.jpg",t="/assets/zim2-c1a94060.jpg",a="/assets/zim3-142343ff.jpg",o="/assets/zim4-0faad767.jpg",p="/assets/zim5-d628d585.jpg";export{s as _,t as a,a as b,o as c,p as d};
